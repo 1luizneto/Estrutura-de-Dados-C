cb820c8384a4fc0bb4a7f4d497ea3b4891141d45
Luiz Neto <luiznetopb@gmail.com>
2023-07-12 15:36:10 -0300

    Add exemplo de Aritmética de ponteiros

---
+++ Vetores/Aritmetrica_de_ponteiros.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int v[5] = {10, 5, 4, 16, 1};
+
+    printf("&v = %p, V = %p\n\n", &v, v);
+
+    for (int i = 0; i < 5; i++){
+        printf("&v[%d] = %p, v[%d] = %d\n", i, &v[i], i, v[i]);
+        printf("(v + %d) = %p, *(v + %d) = %d\n\n", i, (v + i), i, *(v + i));
+    }
+
+    return 0;
+}
